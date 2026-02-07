862943e3c1abad45dadc42d41b6874b5f6388351
jiwonryu68 <jiwonryu68@gmail.com>
2026-02-07 12:05:46 -0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,52 @@
+const CACHE_NAME = 'vendor-order-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+// 설치
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache.map(url => new Request(url, {cache: 'reload'})));
+      })
+      .catch(err => console.log('캐시 실패:', err))
+  );
+  self.skipWaiting();
+});
+
+// 활성화
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+// 요청 가로채기
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+      .catch(() => {
+        return caches.match('./index.html');
+      })
+  );
+});
